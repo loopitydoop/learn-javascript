545ec5c79bad1cf042b34680b8e029fbad697677
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-06-21 22:35:02 +0800

    Completed up to section 3.03

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,12 @@
 // 1. Create two variables, firstCard and secondCard. 
+let firstCard;
+let secondCard;
+
 // Set their values to a random number between 2-11
+firstCard = 5;
+secondCard = 8;
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
+
+console.log(sum);
--- 3. Build a Blackjack game/03. If...else conditionals/index.js
+++ 3. Build a Blackjack game/03. If...else conditionals/index.js
@@ -1,4 +1,14 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 11
+let secondCard = 10
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Do you want to draw a new card?");
+} 
+// strict equality vs loose equality
+else if (sum === 21) {
+    console.log("You've got Blackjack!")
+}
+else {
+    console.log("Bust! You're out of the game")
+}
