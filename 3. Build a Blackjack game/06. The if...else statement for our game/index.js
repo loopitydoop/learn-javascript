99c04dae1dd96c197701adf333741965da5b384a
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-06-23 20:09:43 +0800

    completed up to 3.16

--- 3. Build a Blackjack game/06. The if...else statement for our game/index.js
+++ 3. Build a Blackjack game/06. The if...else statement for our game/index.js
@@ -1,10 +1,17 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 11
 let sum = firstCard + secondCard
 
 // Write the conditional according to these rules:
 
 // if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+if (sum <= 20) {
+    console.log("do you want to draw a new card?");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+    console.log("You're out of the game! 😭");
+}
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
 // else -> "You're out of the game! 😭"
 
