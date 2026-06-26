5f29c0f3e8f5287b9aa6d844de18b5e625e7c114
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-06-26 18:27:04 +0800

    completed up to 5.05

--- 5. Build a Chrome Extension/05. Refactor to addEventListener/index.js
+++ 5. Build a Chrome Extension/05. Refactor to addEventListener/index.js
@@ -2,5 +2,11 @@ function saveLead() {
     console.log("Button clicked!")
 }
 
-
+// addEventListener() workflow is more modern than onclick = function(). It lets you execute multiple functions at once!
+// save element to a variable, as always
+// ig if you do it this way, your JS logic is more separated from the html document as well. E.g. if you change function names here, you don't have to change it in the HTML doc as well.
+let inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!");
+});
 
