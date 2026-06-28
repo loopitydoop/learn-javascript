8cbfe4e9782198d7d95a6f5c95cf7947c9598b12
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-06-29 07:25:32 +0800

    6.03

--- 6. Practice time - part 3/02. Log out items in an array/index.js
+++ 6. Practice time - part 3/02. Log out items in an array/index.js
@@ -2,4 +2,19 @@ let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Cle
 
 // Create a function that takes a single parameter, an array,
 // and logs all the items of the array to the console.
+
+function logArray(array) {
+    for (let i; i < array.length; i++) {console.log(array[i]);}
+}
+
 // Call the function while passing in myCourses as an argument
+logArray(myCourses);
+
+// // either:
+// for (let entry of array) {console.log(entry);}
+// // or
+// for (let [index, entry] of array.entries()) {console.log(entry);}
+// // or
+// array.forEach( (entry) => console.log(entry); )
+// // or
+// for (let i; i < array.length; i++) {console.log(array[i]);}
--- 6. Practice time - part 3/03. save to localStorage/index.js
+++ 6. Practice time - part 3/03. save to localStorage/index.js
@@ -1,3 +1,5 @@
 // Save a value to localStorage
+localStorage.setItem("names", JSON.stringify(["John", "Eren", "Mave"]));
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+console.log( JSON.parse( localStorage.getItem("names") ));
