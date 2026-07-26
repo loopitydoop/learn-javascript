d4b347a147c2df2a297018625b8ecbf9dd637bf8
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-07-26 15:54:14 +0800

    fixed a bug in logArray() where i was not initialised in the for loop, so nothing was being logged on the console.

--- 6. Practice time - part 3/02. Log out items in an array/index.js
+++ 6. Practice time - part 3/02. Log out items in an array/index.js
@@ -2,17 +2,25 @@ let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Cle
 
 // Create a function that takes a single parameter, an array,
 // and logs all the items of the array to the console.
-
-function logArray(array) {
-    for (let i; i < array.length; i++) {console.log(array[i]);}
+// when using for loops with index arithmetic, remember to intiialise i to something (e.g. i = 0)
+function logArray(array)
+{
+    for (let i = 0; i < array.length; i++)
+	{
+		console.log(array[i]);
+	}
 }
 
 // Call the function while passing in myCourses as an argument
+console.log("this is logArray:");
 logArray(myCourses);
 
 // // either:
 // for (let entry of array) {console.log(entry);}
 
+array = myCourses;
+
+console.log("this is for loop by iterating through 'array'");
 for (let array_entry of array) 
 {
 	console.log(array_entry);
@@ -20,6 +28,7 @@ for (let array_entry of array)
 
 // // or
 // for (let [index, entry] of array.entries()) {console.log(entry);}
+console.log("this is for loop by iterating through array.entries");
 for (let [index, entry] of array.entries())
 {
 	console.log(entry);
@@ -29,6 +38,7 @@ for (let [index, entry] of array.entries())
 // array.forEach takes a callback function, into which is passes (1) the current element
 // 	being processed, (2) (optional) the index of the current element being processed, and
 // 	(3) (optional) the original array object that forEach was called upon.
+console.log("this is looping by using array.forEach");
 array.forEach((entry) => {console.log(entry);})
 // // or
 // for (let i; i < array.length; i++) {console.log(array[i]);}
