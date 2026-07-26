84abf1deb7e20a121e582194c38aff2b84f68ca1
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-07-26 16:16:39 +0800

    completed 6.04

--- 6. Practice time - part 3/04. addEventListener and object in array/index.js
+++ 6. Practice time - part 3/04. addEventListener and object in array/index.js
@@ -10,5 +10,25 @@ let data = [
 ]
 
 // Fetch the button from the DOM, store it in a variable
+button = document.querySelector("button");
+
 // Use addEventListener() to listen for button clicks
+button.addEventListener("click", () => 
+	{
+		for (let entry of data)
+		{
+			if (entry.player == "Jane")
+			{
+				console.log(entry.score);
+				break;
+			}
+		}
+	} );
+
 // Log Jane's score when the button is clicked (via data)
+// Gotta remember 
+// (1) (document.querySelector is quite powerful and flexible)
+// (2) the syntax for iterating through elements in an array is "let ELEMENT of ARRAY",
+// 	emphasis on "OF", it's not "in".
+// (3) All that coding in C is paying off, I'm more familiar with using loops and logic. The
+// 	syntax in JS is pretty similar.
