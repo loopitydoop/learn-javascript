a435134b2b39cb1867ec6d3977c116a317ccc9f5
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-07-26 15:29:04 +0800

    completed 6.02

--- 6. Practice time - part 3/02. Log out items in an array/index.js
+++ 6. Practice time - part 3/02. Log out items in an array/index.js
@@ -12,9 +12,23 @@ logArray(myCourses);
 
 // // either:
 // for (let entry of array) {console.log(entry);}
+
+for (let array_entry of array) 
+{
+	console.log(array_entry);
+}
+
 // // or
 // for (let [index, entry] of array.entries()) {console.log(entry);}
+for (let [index, entry] of array.entries())
+{
+	console.log(entry);
+}
 // // or
 // array.forEach( (entry) => console.log(entry); )
+// array.forEach takes a callback function, into which is passes (1) the current element
+// 	being processed, (2) (optional) the index of the current element being processed, and
+// 	(3) (optional) the original array object that forEach was called upon.
+array.forEach((entry) => {console.log(entry);})
 // // or
 // for (let i; i < array.length; i++) {console.log(array[i]);}
