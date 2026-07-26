43ea7db1e4d49524c55fd1d84b602dab36bf7fd1
loopitydoop <153259737+loopitydoop@users.noreply.github.com>
2026-07-26 16:50:32 +0800

    completed 6.05

--- 6. Practice time - part 3/05. Generate sentence/index.js
+++ 6. Practice time - part 3/05. Generate sentence/index.js
@@ -9,5 +9,35 @@
 
 // Use both a for loop and a template string to solve the challenge
 function generateSentence(desc, arr) {
-    
+	arr_str = "";
+	first_item = 1;
+	for (let item of arr)
+	{
+		if (first_item != 1)
+			arr_str += ", ";
+		arr_str += item;
+		first_item = 0;
+	}
+	console.log(`The ${arr.length} ${desc} are ${arr_str}`);
 }
+
+generateSentence("best fruits", ["Apples", "Bananas", "Pears"]);
+
+// the other way is to check for the LAST entry instead to make sure you
+// 	don't print ", " after that.
+function generateSentence2(desc, arr) {
+	baseStr = `The ${arr.length} ${desc} are `;
+	lastIndex = arr.length - 1;
+	for (i = 0; i < arr.length; i++)
+	{
+		baseStr += arr[i];
+		if (i != lastIndex)
+			baseStr += ", ";
+		i++;
+	}
+	console.log("baseStr");
+}
+
+generateSentence("chonkest dawgs", ["garlic dawg", "big dawg", "round dawg"]);
+// there is actualy strict inequality too "!==", where NO type conversion is performed
+// 	before comparing the two values. Loose inequality "!=" performs type conversion.
